0d16542823acce50ee85100c62cb401e6c00dc14
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-08 12:36:15 +0100

    caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 
+A class BasicCache that inherits from BaseCaching and is a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """A BasicCache that inherits from BaseCaching
+        and is a caching system
+    """
+    def __init__(self):
+        """calling the super function"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add key and value to the cache"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get key and value to the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        
+        return self.cache_data[key]
