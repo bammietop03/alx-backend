37afaeddf5e7f8bfd4a489b8f907869a708c5198
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-08 18:33:29 +0100

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -17,18 +17,17 @@ class FIFOCache(BaseCaching):
         """ Add key and value to the cache"""
         if key is None or item is None:
             return
-        
+
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             first_key = next(iter(self.cache_data))
             del self.cache_data[first_key]
             print("DISCARD:", first_key)
-        
+
         self.cache_data[key] = item
 
     def get(self, key):
         """ Get key and value to the cache"""
         if key is None or key not in self.cache_data:
             return None
-        
+
         return self.cache_data[key]
-        
