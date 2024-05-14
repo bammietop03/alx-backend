dbc1cdb77c6d4c87656aecb7308eea474aed25f2
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-14 19:24:29 +0200

    i18n

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" A Flask Application"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Class that contains all language """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def home():
+    """ Home Route"""
+    return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
